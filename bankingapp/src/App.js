3946748a3b5dbbfe1348966003d683ffb73b5ac8
ethanhob <95361850+ethanhob@users.noreply.github.com>
2023-10-18 16:16:54 +1000

    adding heading

--- bankingapp/src/App.js
+++ bankingapp/src/App.js
@@ -7,6 +7,8 @@ import React from 'react';
 export default function App() {
    
       return (
+        <div>
+          <h1>Here is a summary of your accounts</h1>
         <AuthContext.Provider value={{
           // again not sure how to create a global variable
           Authorisation: `Bearer up:yeah:k5W1oFj5K6mfLec2elEjbIlzL14BbrF4ROob1juOc1EkjMIQC9urB8Ojt7ceJx7ulIj4HxASETXsp53ciH3GdMpG5CRxLd8rQgNIptT1RWLkpy79fXk5njQbJEzOCu6O,`   
@@ -16,5 +18,6 @@ export default function App() {
           <Transactions />
         
         </AuthContext.Provider>
+        </div>
       );
 }
