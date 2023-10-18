1092481b146ab1d1865854a131902f485ff12f55
ethanhob <95361850+ethanhob@users.noreply.github.com>
2023-10-18 16:07:47 +1000

    initial commit of files

---
+++ bankingapp/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { AuthContext } from './authrequest';
+import Accounts from './accounts';
+import Transactions from './transactions';
+import React from 'react';
+
+export default function App() {
+   
+      return (
+        <AuthContext.Provider value={{
+          // again not sure how to create a global variable
+          Authorisation: `Bearer up:yeah:k5W1oFj5K6mfLec2elEjbIlzL14BbrF4ROob1juOc1EkjMIQC9urB8Ojt7ceJx7ulIj4HxASETXsp53ciH3GdMpG5CRxLd8rQgNIptT1RWLkpy79fXk5njQbJEzOCu6O,`   
+         }}
+        >
+          <Accounts />
+          <Transactions />
+        
+        </AuthContext.Provider>
+      );
+}
