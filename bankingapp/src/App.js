21385adf55e97808ac6bb571f27d7f7036b0a6e6
ethanhob <95361850+ethanhob@users.noreply.github.com>
2023-12-05 10:40:40 +1000

    commit latest changes
    latest changes

--- bankingapp/src/App.js
+++ bankingapp/src/App.js
@@ -1,49 +1,51 @@
-import "./App.css";
-import { AuthContext } from "./authrequest";
-import Accounts from "./accounts";
-import Transactions from "./transactions";
-import React from "react";
-import { useState, useContext, useEffect, useCallback } from "react";
-import axios from "axios";
+import './App.css';
+import { AuthContext } from './authrequest';
+import Accounts from './accounts';
+import Transactions from './transactions';
+import React from 'react';
+import { useState, useContext, useEffect, useCallback } from 'react';
+import axios from 'axios';
 
 export default function App() {
   const [accountInfo, setAccountInfo] = useState({
-    transactionName: "",
-    transactionBalance: "",
-    savingName: "",
-    savingBalance: "",
+    transactionName: '',
+    transactionBalance: '',
+    savingName: '',
+    savingBalance: '',
   });
-  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiKey=process.env.REACT_APP_API_KEY;
   const auth = useContext(AuthContext);
 
-  // saw the useCallback hook online? but dont know if its what i need here.
-  // what i am trying to do is to pass this function into the transaction component in order to get the transaction
+  // saw this online but dont know if its what i need here.
+  // what i am trying to do is to pass this function into the transaction component in order to get the transaction 
   // balance and savings balance so I can have a running total of balances for each account that adds or subtracts individual transacations
-  const accountAPI = () => {
-    axios.get("https://api.up.com.au/api/v1/accounts", auth).then((res) => {
-      console.log(res);
-      setAccountInfo({
-        transactionName: res.data.data[0].attributes.displayName,
-        transactionBalance: res.data.data[0].attributes.balance.value,
-        savingName: res.data.data[1].attributes.displayName,
-        savingBalance: res.data.data[1].attributes.balance.value,
-      });
-    });
-  };
+    const accountAPI = () => {
+      axios.get('https://api.up.com.au/api/v1/accounts', auth).then((res) => {
+        console.log(res);
+        setAccountInfo({
+          transactionName: res.data.data[0].attributes.displayName, 
+          transactionBalance: res.data.data[0].attributes.balance.value,
+          savingName: res.data.data[1].attributes.displayName, 
+          savingBalance: res.data.data[1].attributes.balance.value,    
+        })
+        })
+    };
 
-  return (
-    <div>
-      <h1>Here is a Summary of your accounts</h1>
-      <AuthContext.Provider
-        value={{
-          headers: {
-            Authorization: `Bearer ${apiKey}`,
-          },
-        }}
-      >
-        <Accounts accountAPI={accountAPI} />
-        <Transactions accountAPI={accountAPI} accountInfo={accountInfo} />
-      </AuthContext.Provider>
-    </div>
-  );
+      return (
+        <div className="App">
+    <h1 className="text-9xl font-bold underline">
+            Here is a Summary of your accounts</h1>
+        <AuthContext.Provider value={
+           {
+            headers: {
+            Authorization: `Bearer ${apiKey}`
+           }
+          }}
+        >
+          <Accounts accountAPI={accountAPI}/>
+          <Transactions accountAPI={accountAPI} accountInfo={accountInfo} />
+        
+        </AuthContext.Provider>
+        </div>
+      );
 }
--- bankingapp/src/transactions.js
+++ bankingapp/src/transactions.js
@@ -2,55 +2,85 @@ import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "./authrequest";
 import axios from "axios";
 
-const Transactions = ({ accountAPI, accountInfo }) => {
+const Transactions = ({ accountAPI }) => {
   const [transactionInfo, setTransactionInfo] = useState({
     name: "",
     amount: "",
     description: "",
   });
 
-  // const [accountInfo, setAccountInfo] = useState({
-  //   transactionName: "",
-  //   transactionBalance: "",
-  //   savingName: "",
-  //   savingBalance: "",
-
-  // })
+  const [accountInfo, setAccountInfo] = useState({
+    transactionName: "",
+    transactionBalance: "",
+    savingName: "",
+    savingBalance: "",
+  });
   const auth = useContext(AuthContext);
 
-  const transactionAccountInfo = ({ accountAPI, accountInfo }) => {
+  const transactionAccountInfo = () => {
     axios.get("https://api.up.com.au/api/v1/transactions", auth).then((res) => {
       // console.log(res);
       setTransactionInfo({
         name: res.data.data[0].attributes.displayName,
-        amount: `$${res.data.data[0].attributes.amount.value}`,
+        amount: res.data.data[0].attributes.amount.value,
         description: res.data.data[0].attributes.description,
+
       });
+      // JSON.stringify(transactionInfo);
+      console.log(transactionInfo);
+      
     });
     // accountAPI();
+  
+    // axios.get("https://api.up.com.au/api/v1/accounts", auth).then((res) => {
+    //   // console.log(res);
+    //   setAccountInfo({
+    //     transactionName: res.data.data[0].attributes.displayName,
+    //     transactionBalance: res.data.data[0].attributes.balance.value,
+    //     savingName: res.data.data[1].attributes.displayName,
+    //     savingBalance: res.data.data[1].attributes.balance.value,
+    //   });
+    // });
+    console.log(accountInfo);
     // its saying this isnt a function but Im not sure why as its the const declared for the api call?
   };
   const savingAccountInfo = ({ accountAPI, accountInfo }) => {
     axios.get("https://api.up.com.au/api/v1/transactions", auth).then((res) => {
       setTransactionInfo({
         name: res.data.data[1].attributes.displayName,
-        amount: `$${res.data.data[1].attributes.amount.value}`,
+        amount: res.data.data[1].attributes.amount.value,
         description: res.data.data[1].attributes.description,
       });
     });
-    // accountAPI();
+    // accountAPI(); so what i need is to is get the value of the totals in each account and display them. 
+    // I maybe need to use the eval function once i push each value into an array with the initial value being the total
+    // 
   };
 
+   const num1 = parseInt(JSON.stringify(transactionInfo.amount))
+   console.log(num1);
+   const num2 = parseInt(JSON.stringify(accountInfo.transactionBalance))
+   console.log(num2);
+   
+   const sum = num1 + num2
+
+  
+
   return (
-    <div>
-      <div>
-        {console.log(accountAPI)};
-        <button onClick={savingAccountInfo}>saving</button>
-        <button onClick={transactionAccountInfo}>transaction</button>
-        <h2>
-          {transactionInfo.amount}: {accountInfo.transactionBalance}{" "}
+    <div className="transaction">
+      <div className="transactionButtons">
+        {/* {console.log(accountAPI)}; */}
+        <button onClick={savingAccountInfo} className="buttons">saving</button>
+        <button onClick={transactionAccountInfo} className="buttons">transaction</button>
+      </div>
+      <div className="transactionInfo">
+        <h2 className={`transactionRunningValue ${transactionInfo.amount ? "positive" : "negative"}`}>
+         Value: {transactionInfo.amount}: {accountInfo.transactionBalance} {sum}
         </h2>
-        <h3>{transactionInfo.description}</h3>
+        <h2>
+         Value: {transactionInfo.amount}: {accountInfo.transactionBalance} {sum}
+        </h2>
+        <h3>Description: {transactionInfo.description}</h3>
       </div>
     </div>
   );
